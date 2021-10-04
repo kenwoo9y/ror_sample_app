8cc48eb7762c9ef796188e9a03d2f676e33b2fe6
kenwoo9y <ei.1px.9.01@gmail.com>
2021-10-04 23:51:18 +0900

    db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_132944) do
+ActiveRecord::Schema.define(version: 2021_09_30_144828) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
